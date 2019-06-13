c90465b5fd836b532e3f3d2306622a4ab84d41df
mschl0ss <mr.schlossberg@gmail.com>
2019-06-12 17:31:29 -0700

    Final commit

--- skeleton/bin/p01_basic_server.rb
+++ skeleton/bin/p01_basic_server.rb
@@ -1,1 +1,20 @@
 require 'rack'
+
+weirdfruitobsession = Proc.new do |env|
+  req = Rack::Request.new(env)
+  res = Rack::Response.new
+  res['Content-Type'] = 'text/html'
+  res.write(req.path)
+  # p env
+  res.finish
+end
+
+
+# Rack::Server.start(
+#   app: <whatever is here is going to be the receiver for this:>.call(allthatdatathatrackcallsenvironment)
+# )
+
+Rack::Server.start(
+  app: weirdfruitobsession,
+  Port: 3000
+)
--- skeleton/lib/controller_base.rb
+++ skeleton/lib/controller_base.rb
@@ -8,25 +8,51 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+    @req = req
+    @res = res
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    raise "well shit partner" if already_built_response?
+    @res.header['location'] = url
+    @res.status = 302
+    @already_built_response = true
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    # @already_built_response ||= false
+    
+    raise "well shit partner" if already_built_response?
+
+    @res['Content-Type'] = content_type
+    @res.write(content)
+    @already_built_response = true
+    
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    first_path = File.dirname(__FILE__)
+    dir_path = File.dirname(first_path)
+    template_path = File.join(dir_path,"views","cats_controller", "#{template_name}.html.erb")
+
+    returned_code = File.read(template_path)
+
+    rendered_html = (ERB.new(returned_code).result(binding))
+
+
+
+    render_content(rendered_html,"text/html")
   end
 
   # method exposing a `Session` object
